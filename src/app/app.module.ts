7b46577f6a478baafb7f65f12f87e93f371c8957
Tiago Farias <tiagofariasnh19@gmail.com>
2023-01-13 18:44:08 -0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/components/app-header/app-header.component';
+import { ConceptButtonComponent } from './core/components/concept-button/concept-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ConceptButtonComponent,
   ],
   imports: [
     BrowserModule
